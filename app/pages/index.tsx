b36086ea929f278e9a4bcce055cecf0c66baf0ae
Ilknur Ultanir <45620986+iibarbari@users.noreply.github.com>
2022-03-19 00:27:06 +0300

    add favicons

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -9,8 +9,13 @@ const Home: NextPage = () => (
     <Head>
       <title>Habits</title>
       <meta content="Habit Tracker" name="description" />
-      <link href="/assets/favicon.ico" rel="shortcut icon" type="image/x-icon" />
-      <link href="/assets/favicon.ico" rel="icon" type="image/x-icon" />
+      <link href="/assets/apple-touch-icon.png" rel="apple-touch-icon" sizes="152x152" />
+      <link href="/assets/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
+      <link href="/assets/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
+      <link href="/assets/site.webmanifest" rel="manifest" />
+      <link color="#5bbad5" href="/assets/safari-pinned-tab.svg" rel="mask-icon" />
+      <meta content="#da532c" name="msapplication-TileColor" />
+      <meta content="#ffffff" name="theme-color" />
     </Head>
 
     <Header />
